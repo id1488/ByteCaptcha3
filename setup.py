a5b0a9633ce6c8d5daedfaadfaf3b115b27dc605
bytecode <oktk0728@gmail.com>
2024-08-17 20:30:00 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
-import setuptools
-with open(r'C:\Users\admin\Проекты\byteCaptcha3\README.md', 'r', encoding='utf-8') as fh:
-	long_description = fh.read()
+from setuptools import setup, find_packages
 
-setuptools.setup(
-	name='byteCaptcha3',
-	version='0.1.0',
-	author='h3xcolor',
-	author_email='oktk0728@gmail.com',
-	description='A powerful captcha generation library for Python',
-	long_description=long_description,
-	long_description_content_type='text/markdown',
-	url='https://github.com/sjskUw/ByteCaptcha3',
-	packages=['byteCaptcha'],
-	install_requires=["Pillow"],
-	include_package_data=True,
-	classifiers=[
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
-	],
-	python_requires='>=3.6',
+setup(
+    name="byteCaptcha3",
+    version="1.0.5",
+    description="A Python library for generating complex CAPTCHAs.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="h3xcolor",
+    author_email="oktk0728@gmail.com",
+    url="https://github.com/sjskUw/ByteCaptcha3",
+    packages=find_packages(),
+    install_requires=[
+        "Pillow",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
